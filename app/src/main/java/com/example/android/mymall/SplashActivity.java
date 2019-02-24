80ca978cf632735673d237ce904d578db5500c3a
atharva121 <atharvasaraf@gmail.com>
2019-02-24 19:05:12 +0530

    2nd commit

--- app/src/main/java/com/example/android/mymall/SplashActivity.java
+++ app/src/main/java/com/example/android/mymall/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.mymall;
 
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        SystemClock.sleep(3000);
+        Intent loginIntent = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
     }
 }
